d18af75a3e509e9778cf2bbaa8352b57cbe6760b
Samuel Gamboa <sd.gamboa.t@gmail.com>
2021-03-26 19:30:16 -0600

    Updated the download_file() function. It no longer updates a file in cache. Instead it prints a message indicating that the file needs to be updated. The user can updated the cache with tools from the BiocFileCache package.

--- R/cache.R
+++ R/cache.R
@@ -18,7 +18,6 @@ metagenomics_cache <- function() {
 #'
 #' @param rname A character vector indicating the resource name (file).
 #' @param url A character vector indicating the url of the resource
-#' @param verbose logical
 #'
 #' @return Character vector of legnth 1, indicating resource name and path
 #' @importFrom BiocFileCache bfcadd
@@ -32,34 +31,42 @@ metagenomics_cache <- function() {
 #' 
 #' 
 #' 
-download_file <- function(rname, url, verbose = FALSE) {
+download_file <- function(rname, url) {
     
     cache_path <-  rappdirs::user_cache_dir(appname="metagenomicsClientR")
     if (!isTRUE(dir.exists(cache_path))) {
-        stop("No cache exists for metagenomicsClientR. Create a cache with the metagenomicsClientR::metagenomics_cache() function.")
+        stop(paste("A cache for metagenomicsClientR does not exit.",
+        "You can create a cache with the metagenomics_cache() function."))
     }
     
     cache <- metagenomics_cache()
     rid <- BiocFileCache::bfcquery(cache, query = rname,
                                    field = "rname", exact = TRUE)$rid
     if (!length(rid)) {
-        if (verbose) message(paste("Downloading", rname))
+        message(paste("Downloading", rname, "to cache..."))
         added_resource <- BiocFileCache::bfcadd(x = cache, 
                                                 rname = rname,
                                                 fpath = url)
-        # rid <- names(added_resource)
         return(added_resource)
     } 
     
     if (!isFALSE(BiocFileCache::bfcneedsupdate(cache, rid))) {
-        if ( verbose ) message( paste("File", rname, "present, but needs update."))
-        updated_resource <- bfcdownload(cache, rid)
-        return(updated_resource)
+        message(paste0("A file named as \"", rname, 
+                      "\" is arlready present in the cache, ",
+                      "but it needs to be updated. ",
+                      "Check bfcneedsupdate(cache, \"",rid,"\").",
+                      " You can overwrite the file with",
+                      " bfcdownload(cache, \"", rid, "\")."))
+        # updated_resource <- bfcdownload(cache, rid)
+        # return(updated_resource)
+        existing_resource <- cache[[rid]]
+        return(existing_resource)
     }
     
     message(
-        paste("A resource named as", rname, 
-              "is already present and up to date in metagenomicsClientR's cache with rid", rid)
+        paste0("A file named as \"", rname, 
+              "\" is already present and up to date in",
+              " cache with the rid \"", rid,".\"")
     )
     
     existing_resource <- cache[[rid]]
